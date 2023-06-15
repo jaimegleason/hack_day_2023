dd06aa89eebc7b75945f79d7aa9936b7a2fe6618
jaimecgleason <jaimecg2@illinois.edu>
2023-06-14 19:02:43 -0500

    changed a background color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 
+
 function App() {
   // eslint-disable-next-line
   const [getMessage, setGetMessage] = useState({})
+
+  const meeting_times = ["10am CST", "11am CST", "1pm CST", 
+                          "2pm CST", "3pm CST", "4pm CST"]
+  const meeting_time = meeting_times[Math.random() % meeting_times.length]
 
   useEffect(()=>{
     axios.get('http://localhost:5000/flask/hello').then(response => {
@@ -26,12 +31,13 @@ function App() {
             <h2 className="card-title">Evelyn Herrera</h2>
             <div className="card-body-2">
               <p><b>Joined State Farm:</b> September 2019</p>
-              <p><b>Title:</b>Technology Analyst</p>
+              <p><b>Title:</b> Technology Analyst</p>
               <p><b>Meeting Time:</b> 2pm CST</p>
               <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><u><b>Meeting Link</b></u></a>
             </div>
   </div>
 </div>
+
       </header>
     </div>
   );
